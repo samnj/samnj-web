a1c335933786057d1d3fe7cc4683c888aaf964f1
sam <7932916+samnj@users.noreply.github.com>
2022-11-19 05:51:41 -0300

    created Navbar, Header and Project components

---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+const livenberg = {
+  description:
+    "A nicer front end for Project Gutenberg with an integrated library.",
+  technologies: "nextjs tailwindcss mysql",
+  imgsrc: "/assets/livenbergImg.png",
+  codeURL: "https://github.com/samnj/livenberg",
+  previewURL: "https://livenberg.vercel.app/",
+}
+
+const livenbergUITest = {
+  description: "Web UI testing automation project for Livenberg.",
+  technologies: "selenium python pytest",
+  imgsrc: "/assets/livenberg-ui-test.png",
+  codeURL: "https://github.com/samnj/livenberg-ui-test",
+  previewURL: null,
+}
+
+const whiteboard = {
+  description: "A simple canvas whiteboard. Work in progress.",
+  technologies: "react tailwindcss",
+  imgsrc: "/assets/whiteboardImg.png",
+  codeURL: "https://github.com/samnj/whiteboard-react",
+  previewURL: "https://samnj.github.io/whiteboard-react/",
+}
+
+export const projects = [livenberg, livenbergUITest, whiteboard]
